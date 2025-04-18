15da75c1ae87bd13fa633584a61c8e118c6e7221
Prateek Mishra <pm921670@gmail.com>
2025-04-18 13:47:07 +0530

    responsive design

--- src/Components/CloudBackground.jsx
+++ src/Components/CloudBackground.jsx
@@ -1,26 +1,24 @@
-// CloudBackground.jsx
 import React from "react";
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const CloudBackground = () => {
   return (
     <div className="relative w-full h-[300px] overflow-hidden bg-white">
-      {/* Social Icons */}
-      <div className="absolute left-0 top-1/2 transform -translate-y-1/2 z-50">
+      {/* Social Icons Sidebar */}
+      <div className="absolute top-1/2 left-0 transform -translate-y-1/2 z-50">
         <div className="flex flex-col space-y-2">
-          <a href="#" className="bg-blue-600 text-white p-2"><i className="fab fa-facebook-f"></i></a>
-          <a href="#" className="bg-orange-600 text-white p-2"><i className="fab fa-instagram"></i></a>
-          <a href="#" className="bg-blue-900 text-white p-2"><i className="fab fa-twitter"></i></a>
-          <a href="#" className="bg-red-600 text-white p-2"><i className="fab fa-youtube"></i></a>
+          <a href="#" className="bg-blue-600 text-white p-2 rounded-r"><FaFacebookF /></a>
+          <a href="#" className="bg-pink-500 text-white p-2 rounded-r"><FaInstagram /></a>
+          <a href="#" className="bg-blue-400 text-white p-2 rounded-r"><FaTwitter /></a>
+          <a href="#" className="bg-red-600 text-white p-2 rounded-r"><FaYoutube /></a>
         </div>
       </div>
 
-      {/* Clouds */}
-      <div className="absolute top-0 w-full h-full z-10">
-        <div className="bg-clouds bg-repeat-x h-full w-full opacity-90"></div>
-      </div>
+      {/* Cloud Layers */}
+      <div className="absolute top-0 left-0 w-full h-full z-10 bg-clouds bg-repeat-x bg-top opacity-90"></div>
 
-      {/* Grass */}
-      <div className="absolute bottom-0 w-full h-[40px] bg-grass z-20"></div>
+      {/* Grass Bottom */}
+      <div className="absolute bottom-0 left-0 w-full h-[50px] bg-grass bg-repeat-x z-20"></div>
     </div>
   );
 };
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,59 +1,98 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import Nature from '../assets/nature.jpg'; 
-import Map from "../assets/map2.png"
+import Map from "../assets/map2.png";
 import Footer from '../Components/Footer';
 import CloudBackground from '../Components/CloudBackground';
 
 const Contact = () => {
-
   return (
     <>
       <Navbar />
+
+      {/* Hero Section with Background */}
       <div
-        className="h-screen w-full bg-cover bg-center bg-no-repeat relative"
+        className="h-auto min-h-screen w-full bg-cover bg-center bg-no-repeat relative"
         style={{ backgroundImage: `url(${Nature})` }}
       >
-      <div className='h-[70%] w-[70%]  absolute top-40 left-40 flex justify-between gap-4'>
-        <div>
-          <img src={Map} alt="" className=' cursor-pointer' />
-        </div>
-        <div className='h-[full] w-[50%] bg-white'>
-          <p className='text-gray-800 font-serif font-semibold text-3xl p-10'>Share Your Query</p>
-          <input type="text" placeholder='Your Name...' name="YourName" className='h-10 w-full border-1 hover:border-orange-600 transition rounded-xl p-4' />
-          <div className='flex justify-between gap-4'>
-          <input type="email" placeholder='Your Email...' name='YourEmail' className='h-10 w-[50%] border-1 hover:border-orange-600 transition rounded-xl mt-4 px-4' />
-          <input type="email" placeholder='Your Email...' name='YourEmail' className='h-10 w-[50%] border-1 hover:border-orange-600 transition rounded-xl mt-4 px-6' />
+        <div className="w-[90%] md:w-[80%] mx-auto mt-20 flex flex-col lg:flex-row items-center justify-between gap-6 lg:gap-10 py-10">
+          {/* Map */}
+          <div className="w-full lg:w-1/2">
+            <img src={Map} alt="Map" className="w-full rounded-lg shadow-lg cursor-pointer" />
           </div>
-          <textarea name="" id="" placeholder='Your Message' className='h-[30%] w-[100%] border-1 hover:border-orange-600 transition rounded-xl mt-4 px-6'/>
-          <button className='h-12 cursor-pointer text-2xl font-semibold   w-full bg-orange-500 text-white'>Send Message</button>
+
+          {/* Form */}
+          <div className="w-full lg:w-1/2 bg-white rounded-lg shadow-xl p-6">
+            <p className="text-gray-800 font-serif font-semibold text-2xl md:text-3xl mb-6">
+              Share Your Query
+            </p>
+
+            <input
+              type="text"
+              placeholder="Your Name..."
+              name="YourName"
+              className="h-10 w-full border border-gray-300 hover:border-orange-600 transition rounded-xl px-4 mb-4"
+            />
+
+            <div className="flex flex-col md:flex-row justify-between gap-4">
+              <input
+                type="email"
+                placeholder="Your Email..."
+                name="YourEmail"
+                className="h-10 w-full md:w-1/2 border border-gray-300 hover:border-orange-600 transition rounded-xl px-4"
+              />
+              <input
+                type="text"
+                placeholder="Your Phone..."
+                name="YourPhone"
+                className="h-10 w-full md:w-1/2 border border-gray-300 hover:border-orange-600 transition rounded-xl px-4"
+              />
+            </div>
+
+            <textarea
+              placeholder="Your Message"
+              className="w-full h-32 border border-gray-300 hover:border-orange-600 transition rounded-xl px-4 py-2 mt-4 resize-none"
+            />
+
+            <button className="h-12 mt-4 w-full bg-orange-500 text-white font-semibold text-xl rounded-xl hover:bg-orange-600 transition">
+              Send Message
+            </button>
+          </div>
         </div>
       </div>
-     
-      </div>
-      <div className='h-[90vh] w-full relative'>
-        <p className='text-4xl font-semibold text-[#ea8b0f] absolute left-[40%] top-10 font-serif'>Contact Us</p>
-        <div className='h-[60%] w-[80%]  absolute top-28 left-40 flex justify-between'>
-          <div>
-            <p className='font-bold text-xl text-gray-800 '>Head Office Address</p>
-            <p className='w-96 mt-2 '>110,111, Unitech Commercial Tower, 2, Block B,
-            Greenwood City, Sector 45, Gurugram, Haryana 122003</p>
+
+      {/* Contact Info Section */}
+      <div className="w-full py-12 px-4 sm:px-8 md:px-20">
+        <p className="text-3xl sm:text-4xl font-semibold text-orange-600 text-center font-serif mb-10">
+          Contact Us
+        </p>
+        <div className="flex flex-col lg:flex-row gap-10 justify-between">
+          <div className="flex-1">
+            <p className="font-bold text-xl text-gray-800 mb-2">Head Office Address</p>
+            <p>
+              110,111, Unitech Commercial Tower, 2, Block B, Greenwood City, Sector 45, Gurugram, Haryana 122003
+            </p>
           </div>
-          <div>
-            <p className='font-bold text-xl text-gray-800 '>Ayodhya Office</p>
-            <p className='w-96 mt-2'>Near Shanti Oil Mill, Housla Nagar Colony, Modha,
-            Ayodhya Dham Uttar Pradesh - 224001</p>
+          <div className="flex-1">
+            <p className="font-bold text-xl text-gray-800 mb-2">Ayodhya Office</p>
+            <p>
+              Near Shanti Oil Mill, Housla Nagar Colony, Modha, Ayodhya Dham Uttar Pradesh - 224001
+            </p>
           </div>
-          <div>
-            <p className='font-bold text-xl text-gray-800 '>CONTACT INFO</p>
-            <p className='w-96 mt-2'>Phone: +91 9318473156<br/>
-Mobile: +91 9971834101
-Email: india@ramrajholidays.in</p>
+          <div className="flex-1">
+            <p className="font-bold text-xl text-gray-800 mb-2">Contact Info</p>
+            <p>
+              Phone: +91 9318473156 <br />
+              Mobile: +91 9971834101 <br />
+              Email: india@ramrajholidays.in
+            </p>
           </div>
         </div>
       </div>
-      <CloudBackground/>
-      <Footer/>
+
+      {/* Cloud Background and Footer */}
+      <CloudBackground />
+      <Footer />
     </>
   );
 };
